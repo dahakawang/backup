e174bc35eafc6cf0c2d6157d0c0d6283e9b6e07d
David <david.kaiqiang.wang@gmail.com>
2017-03-23 20:57:29 +0800

    prune old backups

--- backup.sh
+++ backup.sh
@@ -3,10 +3,12 @@
 DESTINATION_FOLDER="${DESTINATION_ROOT}/${BACKUP_NAME}"
 TS=`date +%Y%m%d_%H%M%S`
 
-FULL_TARGET_FOLDER="${DESTINATION_FOLDER}/${TS}"
-FULL_TEMP_FOLDER="${DESTINATION_FOLDER}/temp/${TS}"
+FULL_TARGET_FOLDER="${DESTINATION_FOLDER}/full/${TS}"
+FULL_TEMP_FOLDER="${DESTINATION_FOLDER}/full/temp/${TS}"
 
 
+# Check last command success
+# $1 - error message to show when error occurs
 function ensure_success() {
     if [ $? != 0 ]; then
         >&2 echo $1
@@ -14,7 +16,31 @@ function ensure_success() {
     fi
 }
 
+
+# Remove old backups
+# $1 - the remote directory that contains backups
+# $2 - the maximum backups to keep
+function prune_backups() {
+ssh root@${DESTINATION_HOST} << EOF
+    set -e
+    if [ ! -e $1 ]; then
+        exit 0
+    fi
+    cd $1
+    backup_cnt=\`ls -d ????????_?????? | wc -l\`
+    remain=\`expr \$backup_cnt - $2\`
+    if [ \$remain -gt 0 ]; then
+        ls -d ????????_?????? | sort | head -\$remain | xargs rm -rf
+    fi
+EOF
+    ensure_success "failed to remove old backups"
+}
+
+# create an full backup
+# No parameter
 function full_backup() {
+    prune_backups ${DESTINATION_FOLDER}/full ${MAX_FULL_BACKUP}
+
 ssh root@${DESTINATION_HOST} << EOF
     mkdir -p ${FULL_TEMP_FOLDER}
 EOF
@@ -28,7 +54,6 @@ ssh root@${DESTINATION_HOST} << EOF
 EOF
     ensure_success "failed to mv fold from ${FULL_TEMP_FOLDER} to ${FULL_TARGET_FOLDER}"
 }
-
 
 if [ "$1" == "full" ]; then
     time full_backup
